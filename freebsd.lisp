e2edcf1620581d899006845b3aad2c64ba96769c
Seiga Kaku <iosevka@naver.com>
2025-02-14 14:13:23 -0300

    Implemented machine-room for FreeBSD

---
+++ freebsd.lisp
@@ -0,0 +1,10 @@
+(in-package #:org.shirakumo.machine-state)
+
+(define-implementation machine-room ()
+  (with-sysctls (("hw.pagesize" page-size :uint64)
+                 ("hw.physmem" physmem :uint64)
+                 ("vm.stats.vm.v_free_count" free :uint64))
+    (let ((page-size (cffi:mem-ref page-size :uint64))
+          (physmem (cffi:mem-ref physmem :uint64))
+          (free (cffi:mem-ref free :uint64)))
+      (values (- physmem (* free page-size)) physmem))))
