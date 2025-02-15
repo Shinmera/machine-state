d4c260589901fc161238eee3673fa15b4674f668
Seiga Kaku <iosevka@naver.com>
2025-02-14 21:07:40 -0300

    [+] Process functions for FreeBSD

--- freebsd.lisp
+++ freebsd.lisp
@@ -46,3 +46,36 @@
             processor ;; There doesn't seem to be a separation between those
             (arch-type)
             (sysctl-string "hw.machine" 32))))
+
+(cffi:defcstruct (rusage :size 144 :conc-name rusage-)
+  (user-time (:struct timeval))
+  (system-time (:struct timeval)))
+
+;;;; https://github.com/freebsd/freebsd-src/blob/main/sys/sys/user.h#L118
+(cffi:defcstruct (kinfo-proc :size 1088 :conc-name kinfo-proc-)
+  (user-id :uint32 :offset 168) ;; ki_uid
+  (group-id :uint32 :offset 180) ;; ki_rgid
+  (resident-set-size :int32 :offset 264) ;; ki_rssize
+  (nice :int8 :offset 389) ;; ki_nice
+  (command-name (:array :char #.(1+ +maxcomlen+)) :offset 447) ;; ki_comm
+  ;; (runtime :uint64 :offset 328) ;; ki_runtime (microseconds)
+  (thread-id :int32 :offset 600)  ;; ki_tid
+  (rusage (:struct rusage) :offset 608)) ;; ki_rusage
+
+(defmacro with-current-process ((proc) &body body)
+  `(with-sysctl (("kern.proc.pid" (getpid)) ,proc '(:struct kinfo-proc))
+     ,@body))
+
+(define-implementation process-room ()
+  (with-current-process (proc)
+    (* (page-size) (kinfo-proc-resident-set-size proc))))
+
+(define-implementation process-priority ()
+  (with-current-process (proc)
+    (process-nice->priority (kinfo-proc-nice proc))))
+
+(define-implementation process-time ()
+  (with-current-process (proc)
+    (let* ((rusage (cffi:foreign-slot-pointer proc '(:struct kinfo-proc) 'rusage))
+           (tv (cffi:foreign-slot-pointer rusage '(:struct rusage) 'user-time)))
+      (timeval->seconds tv))))
