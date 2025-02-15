991427475c6c38db733e83ac96ce3959a98cd421
Seiga Kaku <iosevka@naver.com>
2025-02-15 16:03:28 -0300

    Fix machine-room types and remove some debug print statements

--- freebsd.lisp
+++ freebsd.lisp
@@ -1,12 +1,12 @@
 (in-package #:org.shirakumo.machine-state)
 
 (define-implementation machine-room ()
-  (with-sysctls (("hw.pagesize" page-size :uint64)
+  (with-sysctls (("hw.pagesize" page-size :uint32)
                  ("hw.physmem" physmem :uint64)
-                 ("vm.stats.vm.v_free_count" free :uint64))
-    (let ((page-size (cffi:mem-ref page-size :uint64))
+                 ("vm.stats.vm.v_free_count" free :uint32))
+    (let ((page-size (cffi:mem-ref page-size :uint32))
           (physmem (cffi:mem-ref physmem :uint64))
-          (free (cffi:mem-ref free :uint64)))
+          (free (cffi:mem-ref free :uint32)))
       (values (- physmem (* free page-size)) physmem))))
 
 (define-implementation machine-uptime ()
